28f13aca3590f4e175a7f3268abe6b786a2965c0
Daniel-TheProgrammer <njid18753@gmail.com>
2024-05-01 15:18:10 +0100

    feat: add bgcolor to update numbers

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,48 @@
 <template>
   <div class="box">
-      <div v-for="(item, index) in items" :key="index">
-        <div v-for="(subItem, subIndex) in item" :key="subIndex" class="square">
-          {{ subItem }}
-        </div>
+    <div v-for="(item, index) in items" :key="index">
+      <div
+        v-for="(subItem, subIndex) in item"
+        :key="subIndex"
+        :class="{ square: true, updated: subItem.updated }"
+      >
+        {{ subItem.updated ? subItem.number : subItem.number }}
       </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 
-const items = ref<number[][]>([])
+const items = ref<{ number: number; updated: Boolean }[][]>([])
 
 const generateLists = () => {
-  const newItems: number[][] = []
+  const newItems: { number: number; updated: Boolean }[][] = []
   const numberOfVerticalLists = Math.floor(Math.random() * 100) + 1
   for (let i = 0; i < numberOfVerticalLists; i++) {
-    const horizontalList: number[] = []
+    const horizontalList: { number: number; updated: boolean }[] = []
     const numberOfItems = 10
     for (let j = 0; j < numberOfItems; j++) {
-      horizontalList.push(Math.floor(Math.random() * 100) + 10)
+      horizontalList.push({
+        number: Math.floor(Math.random() * 100) + 20,
+        updated: false
+      })
     }
     newItems.push(horizontalList)
   }
   items.value = newItems
 }
-
 const updateRandomNumber = () => {
-  const randomVerticalIndex = Math.floor(Math.random() * items.value.length)
-  const randomHorizontalIndex = Math.floor(Math.random() * items.value[randomVerticalIndex].length)
-  items.value[randomVerticalIndex][randomHorizontalIndex] = Math.floor(Math.random() * 100)
+  const randomVerticalIndex = Math.floor(Math.random() * 15)
+  const randomHorizontalIndex = Math.floor(Math.random() * 10)
+  if (items.value[randomVerticalIndex][randomHorizontalIndex].updated === true) {
+    return
+  }
+  items.value[randomVerticalIndex][randomHorizontalIndex] = {
+    number: Math.floor(Math.random() * 100) + 20,
+    updated: true
+  }
 }
 
 onMounted(() => {
@@ -59,5 +71,8 @@ onMounted(() => {
 
 .square:hover {
   transform: scale(0.8);
+}
+.updated {
+  background-color: rgba(0, 255, 0, 0.5);
 }
 </style>
